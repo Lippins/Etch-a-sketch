2af7d34b406e096d454ca1ebfc00dc63acc16d0e
Israel Ogunmola <israelogunmola@gmail.com>
2023-08-13 22:15:08 +0100

    Add new shader feature

--- js/main.js
+++ js/main.js
@@ -1,124 +1,152 @@
-function playEtchASketch (){
+const container = document.querySelector(".container");
+let boxes;
+let userChoice;
+let currentColor;
+let inEraseMode = false;
 
-    const container = document.querySelector(".container");
-    let box;
-    let boxes;
-    let userChoice;
-
-    function createBoxes(num){
-        let boxWidth = (450/num);
-        for (i = 0; i< num ; i++){
-            for (j = 0; j< num ; j++){
-                box = document.createElement("div");
-                box.style.width = `${boxWidth}px`;
-                box.style.height = `${boxWidth}px`;
-                container.appendChild(box);
-                boxes = document.querySelectorAll(".container div");
-            }  
-        }
+function createBoxes(num) {
+  let boxWidth = 450 / num;
+  for (i = 0; i < num; i++) {
+    for (j = 0; j < num; j++) {
+      box = document.createElement("div");
+      box.style.width = `${boxWidth}px`;
+      box.style.height = `${boxWidth}px`;
+      container.appendChild(box);
+      boxes = document.querySelectorAll(".container div");
     }
+  }
+}
 
-    createBoxes(16);
+function defaultSketch(color = "black") {
+  currentColor = color;
+  boxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      box.style.backgroundColor = color;
+    });
+  });
+}
 
-    function defaultSketch(color = "black"){
-        boxes.forEach((box)=>{
-            box.addEventListener("mouseenter", ()=>{
-                box.style.backgroundColor = color;
-            });
-        });
+function pickColor() {
+  let selectColor = document.getElementById("pickColor");
+  selectColor.addEventListener("click", () => {
+    let userColorChoice = prompt("kindly Enter A Color Choice Or Hex Value");
+    userColorChoice = userColorChoice.toLowerCase();
+    defaultSketch(userColorChoice);
+    currentColor = userColorChoice;
+  });
+}
+
+function clearPad() {
+  boxes.forEach((box) => {
+    box.style.backgroundColor = "";
+    box.style.opacity = "";
+  });
+}
+
+function reset() {
+  const resetPad = document.getElementById("reset");
+  resetPad.addEventListener("click", clearPad);
+}
+
+function redrawSketchPad() {
+  const redrawPad = document.getElementById("redraw");
+  redrawPad.addEventListener("click", () => {
+    userChoice = parseInt(
+      prompt("How many squares per side do you want to draw?")
+    );
+    if (userChoice < 2 || userChoice > 100) {
+      alert("Kindly enter a number between 2 and 100");
+    } else {
+      clearPad();
+      container.innerHTML = "";
+      createBoxes(userChoice);
+      defaultSketch();
     }
+  });
+}
 
-    defaultSketch();
+function generateRandomColor() {
+  let redValue = Math.floor(Math.random() * 255 + 1);
+  let greenValue = Math.floor(Math.random() * 255 + 1);
+  let blueValue = Math.floor(Math.random() * 255 + 1);
+  let generatedColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+  currentColor = generatedColor;
 
-    function pickColor(){
-        let selectColor = document.getElementById("pickColor");
-        selectColor.addEventListener("click", ()=>{
-            let userColorChoice = prompt("kindly Enter A Color Choice Or Hex Value");
-            let formatUserChoice = userColorChoice.toLowerCase();
-            defaultSketch(formatUserChoice)
-        });
-    }
+  return generatedColor;
+}
 
-    pickColor();
+function randomColorSketcher() {
+  boxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      box.style.backgroundColor = generateRandomColor();
+    });
+  });
+}
 
-    function clearPad(){
-        boxes.forEach((box)=>{
-            box.style.backgroundColor = "white";
-        });
-    }
+function useRandomColorSketcher() {
+  const randomColorPad = document.getElementById("random");
+  randomColorPad.addEventListener("click", randomColorSketcher);
+}
 
-    function reset(){
-        const resetPad = document.getElementById("reset");
-        resetPad.addEventListener("click", ()=>{
-            clearPad();
-        });
-    }
+function erase() {
+  boxes.forEach((box) => {
+    box.removeEventListener("mouseenter", setOpacity);
+  });
 
-    reset();
+  boxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      if (inEraseMode) {
+        box.style.backgroundColor = "";
+        box.style.opacity = "";
+      }
+    });
+  });
+}
 
-    function removeBoxes(){
-        container.innerHTML = "";
-    }
+function erasePad() {
+  const eraser = document.getElementById("eraser");
+  eraser.addEventListener("click", () => {
+    inEraseMode = true;
+    erase();
+  });
+}
 
-    function redrawSketchPad(){
-        const redrawPad = document.getElementById("redraw");
-        redrawPad.addEventListener("click", ()=>{
-            userChoice = parseInt(prompt("How many squares per side do you want to draw?"));
-            if(userChoice < 2 || userChoice > 100){
-                alert("Kindly enter a number between 2 and 100");
-            } else{
-                clearPad();
-                removeBoxes();
-                createBoxes(userChoice);
-                defaultSketch();
-            }
-        });
-    }
+function addOpacity() {
+  const opacityButton = document.querySelector("#shader");
+  opacityButton.addEventListener("click", () => {
+    inEraseMode = false;
+    boxes.forEach((box) => {
+      box.addEventListener("mouseenter", setOpacity);
+    });
+  });
+}
 
-    redrawSketchPad();
+function setOpacity(e) {
+  currentBox = e.target;
+  currentBox.style.backgroundColor = currentColor;
+  if (!currentBox.style.opacity) {
+    currentBox.style.opacity = 0.1;
+  } else if (+currentBox.style.opacity < 1) {
+    currentBox.style.opacity = +currentBox.style.opacity + 0.1;
+  }
+}
 
-   
-    function generateRandomColor(){
-        let generatedColor;
-        let redValue = Math.floor((Math.random()*255) +1);
-        let greenValue = Math.floor((Math.random()*255) +1);
-        let blueValue = Math.floor((Math.random()*255) +1);
-        
-        generatedColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-        return generatedColor;
-    }
+function playEtchASketch() {
+  createBoxes(16);
 
-    function randomColorSketcher(){
-        boxes.forEach((box) =>{
-            box.addEventListener("mouseenter", ()=>{
-                box.style.backgroundColor = `${generateRandomColor()}`;
-            });
-        });
-    }
+  defaultSketch();
 
-    function useRandomColorSketcher(){
-        const randomColorPad = document.getElementById("random");
-        randomColorPad.addEventListener("click", randomColorSketcher);
-    }
+  pickColor();
 
-    useRandomColorSketcher();
+  reset();
 
-    
-    function erase(){
-        boxes.forEach((box) =>{
-            box.addEventListener("mouseenter", ()=>{
-                box.style.backgroundColor = "#FFFFFF";
-            });
-        });
-    }
+  redrawSketchPad();
 
-    function erasePad(){
-        const eraser = document.getElementById("eraser");
-        eraser.addEventListener("click", erase);
-    }
+  useRandomColorSketcher();
 
-    erasePad();
+  addOpacity();
 
+  erasePad();
 }
 
 playEtchASketch();
